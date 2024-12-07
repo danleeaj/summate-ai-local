fdfbac4f6928ec0eea43a367f9454843d3a9d5c5
Daniel Lee <anjie.wav@gmail.com>
2024-12-07 15:37:35 -0800

    Added tests for query response validification

--- utils/validate_query_response.py
+++ utils/validate_query_response.py
@@ -2,34 +2,16 @@
 # from ..utils.validate_query_response import validate_query_response
 from pydantic import BaseModel
 
-class TestModel(BaseModel):
-    response: str
-
-json_with_markdown_sample = """
-```json
-{
-    'response': 'Hello!'
-}
-```
-"""
-
-json_without_markdown_sample = """
-{
-    'response': 'Hello!'
-}
-"""
-
-json_without_whitespace_sample = "{'response': 'Hello!'}"
-
-def clean_string_input(input: str) -> str:
+def clean_string(input: str) -> str:
     clean_input = input.strip().strip("```json").strip("```")
     clean_input = clean_input.replace("'", '"').strip()
     return clean_input
 
-def validate_input(input:str, model:BaseModel) -> dict:
+def validate_query_response(input:str, model:BaseModel) -> BaseModel:
+    clean_response = clean_string(input)
     try:
-        parsed_input = model.model_validate_json(json_data=input, strict=True)
-        return parsed_input
+        parsed_model = model.model_validate_json(json_data=clean_response, strict=True)
+        return parsed_model
     except:
         print("Failed to validate JSON")
         return
