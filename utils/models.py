48a00faba08b1be692e06f7fcea60bf369f7dde1
Daniel Lee <anjie.wav@gmail.com>
2024-12-07 16:07:50 -0800

    Renamed models

--- utils/models.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Model(Enum):
-    """
-    Represents the available LLM options for the debate system.
-    """
-    PHI = "phi3.5"
-    GEMMA = "gemma2"
-    LLAMA = "llama3.2"
