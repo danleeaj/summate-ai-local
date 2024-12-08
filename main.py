5de5748d8f99cc4cdad8e6825e320212a1d1f990
Daniel Lee <anjie.wav@gmail.com>
2024-12-07 16:34:09 -0800

    Removed main

--- main.py
+++
@@ -1,4 +0,0 @@
-from query_model import query_model
-from utils.models import Model
-
-print(query_model("Respond to this message with 'hi'. Structure your response as a json. Do not add any explanations.", Model.GEMMA))
