5992652d5b6be83609f56e4eba300aa458bb806f
Daniel Lee <anjie.wav@gmail.com>
2024-12-08 22:02:35 -0800

    Implemented initiate_debate function

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ from models.response_models.grader_response_model import GraderResponseModel
 from utils.validate_query_response import validate_query_response
 
 from utils.query_large_language_model import query_large_language_model
-from utils.message_generation import initial_prompt_to_grader
+from utils.message_generation import initial_prompt_to_grader, initial_prompt_to_evaluator
+
+from models.debate_models.debate_model import DebateModel
+from models.debate_models.response_model import ResponseModel
+from models.debate_models.round_model import RoundModel
+
+from datetime import datetime, timedelta
 
 test_query = QueryModel(
     rubric_component="Name is stated.",
