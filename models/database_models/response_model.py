0179c9b23c3e88dc27f678aa15aeb99d970f2cb4
Daniel Lee <anjie.wav@gmail.com>
2024-12-30 14:36:17 -0800

    Added identifier to allow for the addition of custom response identifiers.

--- models/database_models/response_model.py
+++ models/database_models/response_model.py
@@ -7,6 +7,8 @@ class ResponseModel(BaseModel):
     Attributes:
         question_id (int): Reference to the associated question.
         response_text (str): The student's response text.
+        identifier (str): A customizable identifier for the response. An example usage would be a student ID.
     """
     question_id: int
     response_text: str
+    identifier: str
